d385a0876dc24a41d383e5f3f8753054de960020
cbohlin <79283698+cbohlin@users.noreply.github.com>
2021-02-18 22:00:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
+var constraints = { video: { facingMode: "user",frameRate: 30 }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
+    cameraTrigger = document.querySelector("#camera--start"),
+    cameraStop = document.querySelector("#camera--stop"),
+    cameraSave = document.querySelector("#camera--disp")
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -12,18 +14,53 @@ function cameraStart() {
         .then(function(stream) {
         track = stream.getTracks()[0];
         cameraView.srcObject = stream;
+    
+
+
+        let mediaRecorder = new MediaRecorder(stream);
+        let vidSave = document.getElementById('camera--disp');
+        let chunks = [];
+
+        cameraTrigger.addEventListener('click', (ev)=>{
+            mediaRecorder.start();
+            console.log('Start');
+            console.log(mediaRecorder.state);
+        })
+
+        cameraStop.addEventListener('click', (ev)=>{
+            mediaRecorder.stop();
+            console.log('Stop');
+            console.log(mediaRecorder.state);
+        });
+
+        mediaRecorder.ondataavailable = function(ev){
+            chunks.push(ev.data);
+            console.log('Gooing');
+        }
+
+        mediaRecorder.onstop = (ev)=>{
+            let blob = new Blob(chunks, { 'type' : 'video/mp4;' });
+            chunks = [];
+            let videoURL = window.URL.createObjectURL(blob);
+            vidSave.src = videoURL;
+        }
+
+
+
+
+
     })
     .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
+        console.error("Error", error);
     });
+
+
+
+    
+    
 }
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
+
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
